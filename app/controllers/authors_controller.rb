b019b62089fa2f0f16dbcf729c5ac05fe2716237
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2020-11-19 21:50:43 +0100

    Failing test for #2

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
   def new
-    
+
   end
 end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,13 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should have text inputs for an author's first name, last name, and homepage" do
+    visit new_author_path
+
+    # these are the standard names given to inputs by the form builder
+    expect(page).to have_field('author[first_name]')
+    expect(page).to have_field('author[last_name]')
+    expect(page).to have_field('author[homepage]')
+  end
 end
