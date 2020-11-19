28c2882b28ec40fcbc3bfa3f107a43baf7885586
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2020-11-19 23:39:46 +0100

    #3 - Add author model

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -8,7 +8,7 @@ describe "New author page", type: :feature do
 
   it "should have text inputs for an author's first name, last name, and homepage" do
     visit new_author_path
-    
+
     # these are the standard names given to inputs by the form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
