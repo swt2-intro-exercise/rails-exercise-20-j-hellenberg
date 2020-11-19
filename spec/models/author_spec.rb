68957ed739ab6b6d12b6b691bdd0d80592775a84
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2020-11-19 23:39:15 +0100

    Failing test for #3

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "is created with correct parameters" do
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+  end
+
+  it "is assigned the right name" do
+    author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+
+    expect(author.name).to eq("Alan Turing")
+  end
+end
